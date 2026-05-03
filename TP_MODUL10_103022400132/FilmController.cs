2f7f1327ceeb2b8d6ca6b51917ee6b0f9b22e20f
FarizqyAkhramN <farizkyakhramnugraha@gmail.com>
2026-05-03 16:14:08 +0700

    Last commit

---
+++ TP_MODUL10_103022400132/FilmController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TP_MODUL10_103022400132;
+namespace TP_MODUL10_103022400132
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FilmController : ControllerBase
+    {
+        private static List<Film> daftarFilm = new List<Film>
+        {
+            new Film("Inception", "Christopher Nolan", "2010", "Sci-Fi", "9.0"),
+            new Film("Interstellar", "Christopher Nolan", "2014", "Sci-Fi", "8.7"),
+            new Film("Parasite", "Bong Joon-ho", "2019", "Thriller", "8.6")
+        };
+
+        [HttpGet]
+        public IEnumerable<Film> Get()
+        {
+            return daftarFilm;
+        }
+
+        [HttpGet("{index}")]
+        public Film Get(int index)
+        {
+            return daftarFilm[index];
+        }
+
+        [HttpPost]
+        public void Post([FromBody] Film filmBaru)
+        {
+            daftarFilm.Add(filmBaru);
+        }
+
+        [HttpDelete("{index}")]
+        public void Delete(int index)
+        {
+            daftarFilm.RemoveAt(index);
+        }
+    }
+}
--- TP_MODUL10_103022400132/obj/Debug/net10.0/TP_MODUL10_103022400132.AssemblyInfo.cs
+++ TP_MODUL10_103022400132/obj/Debug/net10.0/TP_MODUL10_103022400132.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TP_MODUL10_103022400132")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c180795729c733081b66c44691b359bfa4afd020")]
 [assembly: System.Reflection.AssemblyProductAttribute("TP_MODUL10_103022400132")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TP_MODUL10_103022400132")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
